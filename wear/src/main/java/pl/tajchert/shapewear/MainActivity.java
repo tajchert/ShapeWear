0fdf1f98ee0882a501effd8f93ed5e0bab657b58
Michal Tajchert <michal.tajchert@polidea.com>
2015-02-14 21:26:53 +0100

    Show results in sample, and use size detection.

--- wear/src/main/java/pl/tajchert/shapewear/MainActivity.java
+++ wear/src/main/java/pl/tajchert/shapewear/MainActivity.java
@@ -6,19 +6,37 @@ import android.util.Log;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+    private TextView screenShapeText;
+    private TextView screenSizeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Would give us wrong result of "getIsRound()", on first run here
+        screenShapeText = (TextView) findViewById(R.id.ScreenShape);
+        screenSizeText = (TextView) findViewById(R.id.ScreenSize);
 
+
+        //"getIsRound()" would give us wrong result (exception), on first run here
 
         ShapeWear.setOnShapeChangeListener(new ShapeWear.OnShapeChangeListener() {
             @Override
             public void shapeDetected(boolean isRound) {
                 //There is fine.
-                Log.d(TAG, "shapeDetected :" + isRound);
+                Log.d(TAG, "shapeDetected isRound:" + isRound);
+                Log.d(TAG, "shapeDetected getShape:" + ShapeWear.getShape());
+                if(screenShapeText != null) {
+                    screenShapeText.setText("isRound: " + isRound);
+                }
+            }
+        });
+        ShapeWear.setOnSizeChangeListener(new ShapeWear.OnSizeChangeListener() {
+            @Override
+            public void sizeDetected(int widthPx, int heightPx) {
+                Log.d(TAG, "sizeDetected w:" + widthPx + ", h:" + heightPx);
+                if(screenSizeText != null) {
+                    screenSizeText.setText("size: " + widthPx + "x" + heightPx);
+                }
             }
         });
         ShapeWear.initShapeWear(this);
@@ -27,10 +45,10 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        //Would give us wrong result of "getIsRound()", on first run here
-        Log.d(TAG, "onResume shape: " + ShapeWear.getShape());
+        //"getIsRound()" would give us wrong result (exception), on first run here
+        Log.d(TAG, "onResume shape: " + ShapeWear.getShape()); //Exception proof method to get shape, would give us ShapeWear.SHAPE_UNSURE if not determined shape yet
         try {
-            //Woul be thrown on first run as shape will be not detected when calling onResume
+            //Would be thrown on first run as shape will be still not detected when calling onResume()
             Log.d(TAG, "onResume shape: " + ShapeWear.isRound());
         } catch (Exception e) {
             Log.e(TAG, "Exception from isRound: " + e.getLocalizedMessage());
